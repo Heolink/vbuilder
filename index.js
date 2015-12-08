b076ee221ec433f9849842b18b01225846d2fd44
Heolink <heolink@gmail.com>
2015-12-08 19:47:30 +0100

    swtich to commander

--- index.js
+++ index.js
@@ -1,266 +1,29 @@
 #! /usr/bin/env node
-var colors = require('colors');
-var commandLineArgs = require('command-line-args');
-var prompt = require('prompt');
-var fs = require('fs');
-var async = require('async');
+var ip = require('ip');
+var program = require('commander');
+var app = require('./lib/app');
 
-module.exports = fs.existsSync || function existsSync(filePath){
-        try{
-            fs.statSync(filePath);
-        }catch(err){
-            if(err.code == 'ENOENT') return false;
-        }
-        return true;
-    };
-
-var cli = commandLineArgs([
-    { name: 'verbose', alias: 'v', type: Boolean },
-    { name: 'vhost', type: String, defaultOption: true },
-    { name: 'ip', type: String, defaultValue: '192.168.89.29' },
-    { name: 'box', type: String, defaultValue: 'ubuntu/trusty64'},
-    { name: 'mysql', alias: 'm', type: Boolean, defaultValue: false},
-    { name: 'all', type: Boolean, defaultValue: false},
-    { name: 'composer', alias: 'c', type: Boolean, defaultValue: false},
-    { name: 'node', type: Boolean, defaultValue: false},
-    { name: 'redis', type: Boolean, defaultValue: false},
-    { name: 'git', type: Boolean, defaultValue: false},
-    { name: 'bower', type: Boolean, defaultValue: false},
-    { name: 'gulp', type: Boolean, defaultValue: false},
-])
-var options = cli.parse()
-var currentPath = process.cwd();
-
-var verbose = function(string)
-{
-    if( options.verbose ) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        console.info( '[INFO] ' + string.yellow, args );
-    }
-}
-
-verbose('Start process, options : ', options);
-
-if( !options.hasOwnProperty('vhost') || !options.vhost ) {
-    return console.error('[ERROR] vhost require'.red);
-}
-
-if(fs.existsSync( currentPath + '/Vagrantfile')) {
-    return console.error('[ERROR] Vagrantfile already exists'.red);
-}
-
-if(fs.existsSync( currentPath + '/bootstrap.sh')) {
-    return console.error('[ERROR] bootstrap.sh already exists'.red);
-}
-
-var fileBootstrap = __dirname + '/templates/bootstrap.sh.template';
-var fileMysql = __dirname + '/templates/mysql.sh.template';
-var fileNode = __dirname + '/templates/node.sh.template';
-var fileGulp = __dirname + '/templates/gulp.sh.template';
-var fileBower = __dirname + '/templates/bower.sh.template';
-var fileRedis = __dirname + '/templates/redis.sh.template';
-var fileComposer = __dirname + '/templates/composer.sh.template';
-var fileVhost = __dirname + '/templates/vhost.sh.template';
-var fileVagrant = __dirname + '/templates/Vagrantfile.template';
-var processFunction = [];
-
-if( options.all ) {
-    for( var k in options ) {
-        if( k == 'verbose' ) {
-            continue;
-        }
-        if( options[k] === false ) {
-            options[k] = true;
-        }
-    }
-}
-
-if( options.gulp || options.bower ) {
-    verbose('Set node to true');
-    options.node = true;
-}
-
-verbose('Read file bootstrap');
-fs.readFile(fileBootstrap, 'utf8', function (err,bootstrap) {
-    if (err) {
-        return console.log('[ERROR]' + err.red);
-    }
-
-    if( options.composer ) {
-        verbose('composer set to true');
-        var composerProcess = function(callback)
-        {
-            fs.readFile(fileComposer, 'utf8', function (err, composer) {
-                if (err) {
-                    return console.log('[ERROR]' + err.red);
-                }
-                callback(null, composer);
-            });
-        }
-        processFunction.push(composerProcess);
-    }
-
-    if( options.redis ) {
-        verbose('redis set to true');
-        var redisProcess = function(callback)
-        {
-            fs.readFile(fileRedis, 'utf8', function (err, redis) {
-                if (err) {
-                    return console.log('[ERROR]' + err.red);
-                }
-                callback(null, redis);
-            });
-        }
-        processFunction.push(redisProcess);
-    }
-
-    if( options.node ) {
-        verbose('node set to true');
-        var nodeProcess = function(callback)
-        {
-            fs.readFile(fileNode, 'utf8', function (err, node) {
-                if (err) {
-                    return console.log('[ERROR]' + err.red);
-                }
-                callback(null, node);
-            });
-        }
-        processFunction.push(nodeProcess);
-
-        if( options.gulp ) {
-            verbose('gulp set to true');
-            var gulpProcess = function(callback)
-            {
-                fs.readFile(fileGulp, 'utf8', function (err, gulp) {
-                    if (err) {
-                        return console.log('[ERROR]' + err.red);
-                    }
-                    callback(null, gulp);
-                });
-            }
-            processFunction.push(gulpProcess);
-        }
-
-        if( options.bower ) {
-            verbose('bower set to true');
-            var bowerProcess = function(callback)
-            {
-                fs.readFile(fileBower, 'utf8', function (err, bower) {
-                    if (err) {
-                        return console.log('[ERROR]' + err.red);
-                    }
-                    callback(null, bower);
-                });
-            }
-            processFunction.push(bowerProcess);
-        }
-
-    }
-
-    if( options.mysql ) {
-        verbose('mysql set to true');
-        var mysqlProcess = function(callback)
-        {
-            fs.readFile(fileMysql, 'utf8', function (err, mysql) {
-                if (err) {
-                    return console.log('[ERROR]' + err.red);
-                }
-                var baseName, basePassword;
-                var schema = {
-                    properties: {
-                        baseName: {
-                            description: 'Database name',
-                            required: true
-                        },
-                        basePassword: {
-                            description: 'Database password',
-                            required: true,
-                            hidden: true
-                        }
-                    }
-                };
-                prompt.start();
-                prompt.get(schema, function(err, result){
-                    baseName = result.baseName;
-                    basePassword = result.basePassword;
-
-                    mysql = mysql.replace(/###MYSQLPASSWORD###/g, basePassword);
-                    mysql = mysql.replace(/###MYSQLDATABASE###/g, baseName);
-
-                    callback(null, mysql);
-
-                });
-            })
-        }
-
-        processFunction.push(mysqlProcess);
-    }
-
-    if( options.vhost ) {
-        var vhostProcess = function(callback) {
-            fs.readFile(fileVhost, 'utf8', function (err, vhost) {
-                if (err) {
-                    return console.log('[ERROR]' + err.red);
-                }
-                var publicWeb;
-                var schema = {
-                    properties: {
-                        publicWeb: {
-                            description: 'Obtionel, sous dossier web'
-                        },
-                    }
-                };
-                prompt.start();
-                prompt.get(schema, function (err, result) {
-                    publicWeb = result.publicWeb;
-
-                    vhost = vhost.replace(/###HOST###/g, options.vhost);
-                    vhost = vhost.replace(/###PUBLICWEB###/g, publicWeb);
-
-                    callback(null, vhost);
-
-                });
+var defaultIp = '192.168.89.29';
+var defaultBox = 'ubuntu/trusty64';
 
 
-            })
-        }
-        processFunction.push(vhostProcess);
-    }
-
-    async.series( processFunction, function(err, results) {
-        // all tasks have completed, run any post-processing.
-        if( results ) {
-            bootstrap += results.join("\n");
-
-            fs.readFile(fileVagrant, 'utf8', function (err, vagrant) {
-                if (err) {
-                    return console.log('[ERROR]' + err.red);
-                }
-
-                vagrant = vagrant.replace(/###BOX###/g, options.box);
-                vagrant = vagrant.replace(/###IP###/g, options.ip);
-
-                fs.writeFile('Vagrantfile', vagrant, function(err) {
-                    if(err) {
-                        return console.log('[ERROR] ' + err.red);
-                    }
-
-                    fs.writeFile('bootstrap.sh', bootstrap, function(err) {
-                        if(err) {
-                            fs.unlink('Vagrantfile');
-                            return console.log('[ERROR] ' + err.red);
-                        }
-
-                        return console.log('Fichier générer'.green);
-                    });
-
-                });
-
-            });
-
-        }
-    });
-
-});
+program
+    .version('1.0.0')
+    .command('build')
+    .option('-v, --verbose', 'vervbose mode')
+    .option('-i, --ip <ip>', 'ip to Vagrant machine default : ' + defaultIp, ip.isV4Format, defaultIp)
+    .option('-b, --box <box>', 'box Vagrant default : ' + defaultBox, defaultBox)
+    .option('-h, --host <host>', 'install apache2 with virtualhost')
+    .option('-m, --mysql', 'install mysql')
+    .option('-c, --composer', 'install composer')
+    .option('-n, --node', 'install node')
+    .option('-r, --redis', 'install redis')
+    .option('-g, --git', 'install git')
+    .option('-b, --bower', 'install bower, if use install node')
+    .option('-G, --gulp', 'install gulp, if use install node')
+    .option('-t, --test', 'test mode, no save file')
+    .option('-A, --all', 'use all options')
+    .action(app)
 
 
+program.parse(process.argv);
--- readme.md
+++ readme.md
@@ -1,23 +1,40 @@
 #vbuilder
-Create Vagrantfile and bootrstap.sh with setup apache2 and more
+Create Vagrantfile and bootrstap.sh in current folder
 
 
 ###Installation
     npm install
     npm link
-    vbuilder monsite.dev
-    or
-    vbuilder --ip 192.168.1.90 --node monsite.dev
     
-###Params
-    ip        : default 192.168.89.29
-    box       : default ubuntu/trusty64
-    all       : default false | set true fro all
-    mysql     : default false
-    composer  : default false
-    node      : default false
-    bower     : default false | node required
-    gulp      : default false | node required
-    redis     : default false
-    git       : default false
+###Usage
+    Usage: build [options]
+    
+      Options:
+    
+        -h, --help         output usage information
+        -v, --verbose      vervbose mode
+        -i, --ip <ip>      ip to Vagrant machine default : 192.168.89.29
+        -b, --box <box>    box Vagrant default : ubuntu/trusty64
+        -h, --host <host>  install apache2 with virtualhost
+        -m, --mysql        install mysql
+        -c, --composer     install composer
+        -n, --node         install node
+        -r, --redis        install redis
+        -g, --git          install git
+        -b, --bower        install bower, if use install node
+        -G, --gulp         install gulp, if use install node
+        -t, --test         test mode, no save file
+        -A, --all          use all options
+
+###Example
+    //minimal
+    vbuilder build
+    
+    //with apache2/php5 and virtualhost site.com
+    vbuilder build -h site.com
+    
+    //with all option and vhost
+    vbuilder build -A -h site.com
+    
+
     
